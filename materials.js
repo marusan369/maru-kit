a648044fcdb294d9c99610c9a0c6dce170415677
marusan369 <ojineko369@gmail.com>
2026-09-23 16:39:32 +0900

    Add files via upload

---
+++ materials.js
@@ -0,0 +1,46 @@
+const materials = [
+    {
+        name: "時計回り（ゆっくり）",
+        video: "preview/時計回り_ゆっくり.mp4",
+        zip: "zip/tokeimawari_yukkuri.zip",
+        available: true
+    },
+    {
+        name: "時計回り（早い）",
+        video: "preview/時計回り_早い.mp4",
+        zip: "zip/tokeimawari_hayai.zip",
+        available: true
+    },
+    {
+        name: "反時計回り（ゆっくり）",
+        video: "preview/反時計回り_ゆっくり.mp4",
+        zip: "zip/hantokeimawari_yukkuri.zip",
+        available: true
+    },
+    {
+        name: "反時計回り（早い）",
+        video: "preview/反時計回り_早い.mp4",
+        zip: "zip/hantokeimawari_hayai.zip",
+        available: true
+    },
+    {
+        name: "バウンド移動（ゆっくり）",
+        video: "preview/バウンド移動_ゆっくり.mp4",
+        zip: "zip/bound_idou_yukkuri.zip",
+        available: true
+    },
+    {
+        name: "バウンド移動（はやい）",
+        video: "preview/バウンド移動_早い.mp4",
+        zip: "zip/bound_idou_hayai.zip",
+        available: true
+    },
+
+
+
+    // 5番目以降（新しい素材ができたらここに書き足すだけでOK！）
+    { available: false },
+    { available: false },
+    { available: false },
+    { available: false }
+];
