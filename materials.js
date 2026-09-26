5097b1f56bcd10438ce7f11c57b4f153e4188482
marusan369 <ojineko369@gmail.com>
2026-09-26 23:38:16 +0900

    Add files via upload

--- materials.js
+++ materials.js
@@ -1,109 +1,109 @@
 const materials = [
     {
         name: "時計回り（ゆっくり）",
-        video: "preview/時計回り_ゆっくり.mp4",
+        video: "preview/時計回り_ゆっくり.png",
         zip: "zip/tokeimawari_yukkuri.zip",
         available: true
     },
     {
         name: "時計回り（早い）",
-        video: "preview/時計回り_早い.mp4",
+        video: "preview/時計回り_早い.png",
         zip: "zip/tokeimawari_hayai.zip",
         available: true
     },
     {
         name: "反時計回り（ゆっくり）",
-        video: "preview/反時計回り_ゆっくり.mp4",
+        video: "preview/反時計回り_ゆっくり.png",
         zip: "zip/hantokeimawari_yukkuri.zip",
         available: true
     },
     {
         name: "反時計回り（早い）",
-        video: "preview/反時計回り_早い.mp4",
+        video: "preview/反時計回り_早い.png",
         zip: "zip/hantokeimawari_hayai.zip",
         available: true
     },
     {
         name: "横回転（ゆっくり）",
-        video: "preview/横回転_ゆっくり.mp4",
+        video: "preview/横回転_ゆっくり.png",
         zip: "zip/yoko_kaiten_yukkuri.zip",
         available: true
     },
     {
         name: "横回転（はやい）",
-        video: "preview/横回転_早い.mp4",
+        video: "preview/横回転_早い.png",
         zip: "zip/yoko_kaiten_hayai.zip",
         available: true
     },
     {
         name: "縦回転（ゆっくり）",
-        video: "preview/縦回転_ゆっくり.mp4",
+        video: "preview/縦回転_ゆっくり.png",
         zip: "zip/tate_kaiten_yukkuri.zip",
         available: true
     },
     {
         name: "縦回転（はやい）",
-        video: "preview/縦回転_早い.mp4",
+        video: "preview/縦回転_早い.png",
         zip: "zip/tate_kaiten_hayai.zip",
         available: true
     },
     {
         name: "上下移動",
-        video: "preview/上下移動.mp4",
+        video: "preview/上下移動.png",
         zip: "zip/jouge_idou.zip",
         available: true
     },
     {
         name: "左右移動",
-        video: "preview/左右移動.mp4",
+        video: "preview/左右移動.png",
         zip: "zip/sayuu_idou.zip",
         available: true
     },
     {
         name: "ゆれる（上）",
-        video: "preview/ゆれる_上.mp4",
+        video: "preview/ゆれる_上.png",
         zip: "zip/yureru_ue.zip",
         available: true
     },
     {
         name: "ゆれる（下）",
-        video: "preview/ゆれる_下.mp4",
+        video: "preview/ゆれる_下.png",
         zip: "zip/yureru_sita.zip",
         available: true
     },
     {
         name: "くるくる揺れ",
-        video: "preview/くるくる揺れ.mp4",
+        video: "preview/くるくる揺れ.png",
         zip: "zip/kurukuru_yure.zip",
         available: true
     },
     {
         name: "ぴょんぴょん",
-        video: "preview/ぴょんぴょん.mp4",
+        video: "preview/ぴょんぴょん.png",
         zip: "zip/pyon_pyon.zip",
         available: true
     },
     {
         name: "ふわふわ",
-        video: "preview/ふわふわ.mp4",
+        video: "preview/ふわふわ.png",
         zip: "zip/huwa_huwa.zip",
         available: true
     },
     {
         name: "鼓動",
-        video: "preview/鼓動.mp4",
+        video: "preview/鼓動.png",
         zip: "zip/kodou.zip",
         available: true
     },
     {
         name: "バウンド移動（ゆっくり）",
-        video: "preview/バウンド移動_ゆっくり.mp4",
+        video: "preview/バウンド移動_ゆっくり.png",
         zip: "zip/bound_idou_yukkuri.zip",
         available: true
     },
     {
         name: "バウンド移動（はやい）",
-        video: "preview/バウンド移動_早い.mp4",
+        video: "preview/バウンド移動_早い.png",
         zip: "zip/bound_idou_hayai.zip",
         available: true
     },
